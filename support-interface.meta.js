e7679b9b45ae48ce88222ee09858252cc62f00dd
joao <joao@hipp.health>
2026-03-17 11:15:03 -0300

    bump meta to v2.2.0

--- support-interface.meta.js
+++ support-interface.meta.js
@@ -1,13 +1,8 @@
 // ==UserScript==
 // @name         Support Intercom Interface
 // @namespace    https://app.intercom.com
-// @version      2.0.0
+// @version      2.2.0
 // @author       joao@hipp.health, guilherme@hipp.health
 // @updateURL    https://raw.githubusercontent.com/joao-hipp/intercom-support-tools/main/support-interface.meta.js
 // @downloadURL  https://raw.githubusercontent.com/joao-hipp/intercom-support-tools/main/support-interface.user.js
 // ==/UserScript==
-```
-
-And the install link for the README and your team:
-```
-https://raw.githubusercontent.com/joao-hipp/intercom-support-tools/main/support-interface.user.js
